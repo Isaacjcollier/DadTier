1637aa68ac8becd0f978792706a52601df663b05
Isaac Collier <chunkywombat@Isaacs-MacBook-Pro.local>
2020-04-22 16:07:15 -0700

    Updates

---
+++ precache-manifest.946e63b46cb065e82f292375e78f546c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb63d781b8dabe5f5d6fe80b1cbf38f",
+    "url": "/DadTier/index.html"
+  },
+  {
+    "revision": "e814cbb13bec49147816",
+    "url": "/DadTier/static/css/main.ae30cc6a.chunk.css"
+  },
+  {
+    "revision": "544b1fb60635596b516c",
+    "url": "/DadTier/static/js/2.07330c03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DadTier/static/js/2.07330c03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e814cbb13bec49147816",
+    "url": "/DadTier/static/js/main.8f83ba1d.chunk.js"
+  },
+  {
+    "revision": "c318e28a5c85e31f8f85",
+    "url": "/DadTier/static/js/runtime-main.7820ce23.js"
+  },
+  {
+    "revision": "d9721036f2d3d0a872ed0c53b7ae326c",
+    "url": "/DadTier/static/media/collection.d9721036.jpg"
+  },
+  {
+    "revision": "520d97e89f5b99939c8466692faa0a0d",
+    "url": "/DadTier/static/media/dadtierBrand.520d97e8.png"
+  },
+  {
+    "revision": "8acd9399ac3314619efdc6a6faaeab6a",
+    "url": "/DadTier/static/media/dance.8acd9399.gif"
+  },
+  {
+    "revision": "dd0711bf79d0c874a81b9028db87816d",
+    "url": "/DadTier/static/media/discordLogo.dd0711bf.png"
+  },
+  {
+    "revision": "456b28ed9a3ddc99a6718a07d2194e7b",
+    "url": "/DadTier/static/media/headerLogo.456b28ed.png"
+  },
+  {
+    "revision": "097b77494452f9352630bcf79697db4d",
+    "url": "/DadTier/static/media/instagramLogo.097b7749.png"
+  },
+  {
+    "revision": "486009cce43c8196126ad78302f4626b",
+    "url": "/DadTier/static/media/redbubbleLogo.486009cc.png"
+  },
+  {
+    "revision": "c651441c35a80690348471d300e4e89b",
+    "url": "/DadTier/static/media/tv.c651441c.png"
+  },
+  {
+    "revision": "e15c59fc0a45df99652a05a29538a629",
+    "url": "/DadTier/static/media/twitchAffiliate.e15c59fc.jpg"
+  },
+  {
+    "revision": "3d74e8bfd4ef7985f7529bb9f7650eca",
+    "url": "/DadTier/static/media/twitterLogo.3d74e8bf.png"
+  },
+  {
+    "revision": "eca280fe0337c9292c79458f75b5c170",
+    "url": "/DadTier/static/media/youtubeButton.eca280fe.jpg"
+  },
+  {
+    "revision": "c411682ae9f3688a1c662f45bf9a685a",
+    "url": "/DadTier/static/media/youtubeLogo.c411682a.png"
+  }
+]);
